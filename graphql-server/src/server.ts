2b691ca50aec0b59cdf0aba687760156550c4861
Vinay kumar <vinayrahar1991@gmail.com>
2020-06-08 15:08:10 +0530

    Remove dist folder server files

---
+++ graphql-server/src/server.ts
@@ -0,0 +1,16 @@
+import { ApolloServer } from 'apollo-server';
+import depthLimit from 'graphql-depth-limit';
+import schema from './schema';
+
+const server = new ApolloServer({
+  schema,
+  validationRules: [depthLimit(7)],
+  playground: true,
+  formatError: err => {
+    return err;
+  }
+ });
+
+server.listen({port: 3000}).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
